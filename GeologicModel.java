6e3ee38c0e218c3aee6dc379ecc5478cb8ce3abb
joshuaadampoirier <josh.poirier@gmail.com>
2014-10-26 13:57:08 -0600

    with some documentation

--- GeologicModel.java
+++ GeologicModel.java
@@ -104,23 +104,23 @@ public class GeologicModel {
 	/* build sedimentary model */
 	public void buildSed() {
 		// approximate infinity with 30000 units
-		sed.add(new ModelCoord(-300000, 0));
-		sed.add(new ModelCoord(300000, 0));
-		sed.add(new ModelCoord(300000, spatialLine.getEndpoint(false).getZ()));
+		sed.add(new ModelCoord(-100000, 0));
+		sed.add(new ModelCoord(100000, 0));
+		sed.add(new ModelCoord(100000, spatialLine.getEndpoint(false).getZ()));
 		sed.add(new ModelCoord(spatialLine.getLength()/2, spatialLine.getEndpoint(false).getZ()));
 		sed.add(new ModelCoord(spatialLine.getLength()/2, spatialLine.getEndpoint(true).getZ()));
-		sed.add(new ModelCoord(-300000, spatialLine.getEndpoint(true).getZ()));
+		sed.add(new ModelCoord(-100000, spatialLine.getEndpoint(true).getZ()));
 	}
 	
 	/* build basement model */
 	public void buildBase() {
 		// approximate infinity with 30000 units
-		base.add(new ModelCoord(-300000, spatialLine.getEndpoint(true).getZ()));
+		base.add(new ModelCoord(-100000, spatialLine.getEndpoint(true).getZ()));
 		base.add(new ModelCoord(spatialLine.getLength()/2, spatialLine.getEndpoint(true).getZ()));
 		base.add(new ModelCoord(spatialLine.getLength()/2, spatialLine.getEndpoint(false).getZ()));
-		base.add(new ModelCoord(300000, spatialLine.getEndpoint(false).getZ()));
-		base.add(new ModelCoord(300000, 300000));
-		base.add(new ModelCoord(-300000, 300000));
+		base.add(new ModelCoord(100000, spatialLine.getEndpoint(false).getZ()));
+		base.add(new ModelCoord(100000, 30000));
+		base.add(new ModelCoord(-100000, 30000));
 	}
 	
 	public void printSed() {
@@ -138,7 +138,7 @@ public class GeologicModel {
 	public void calcGrav() {
 		for (int i = 0; i < stations.size(); i++) {
 			System.out.println(forwardGravity(i)/0.00001);
-			stations.get(i).setGz(forwardGravity(i));
+			stations.get(i).setGz(forwardGravity(i)/0.00001);
 		}
 	}
 	
@@ -166,7 +166,7 @@ public class GeologicModel {
 		sumZ = 0;
 		for (int i = 0; i < b.size(); i++) {
 			//System.out.println("Computing line integral along Basement edge " + i);
-			sumZ += lineIntegral(i,s);
+			sumZ += lineIntegral(i,b);
 		}
 		gB = 2 * G * rhoBase * sumZ;
 		//System.out.println("Computed basement Gz is " + gB);
@@ -196,19 +196,19 @@ public class GeologicModel {
 		//System.out.println("Line integral coords are (" + xi + "," + zi + ") and (" + xj + "," + zj + ")");
 		
 		// compute components of the line integral
-		double theta_i = Math.atan(zi / xi);
-		double theta_ip1 = Math.atan(zj / xj);
+		double theta_i = Math.atan(zi / Math.abs(xi));
+		double theta_ip1 = Math.atan(zj / Math.abs(xj));
 		double A = (xj - xi) * (xi*zj - xj*zi) / (Math.pow((xj-xi), 2) + (Math.pow((zj-zi), 2)));
 		//System.out.println("A = " + A);
-		if (xi == xj) Math.abs(B = (zj - zi) / 0.00001);
-		else B = Math.abs((zj - zi) / (xj - xi));
-		//System.out.println("B = " + B);
+		if (xi == xj) B = (zj - zi) / 0.0001;
+		else B = (zj - zi) / (xj - xi);
+		System.out.println("zj-zi = " + (zj-zi) + "B = " + B);
 		double ri_2 = Math.pow(xi, 2) + Math.pow(zi, 2);
 		double rip1_2 = Math.pow(xj, 2) + Math.pow(zj, 2);
 		//System.out.println("ri = " + ri_2 + " ri+1 = " + rip1_2);
 		
 		// compute and return value of the line integral along the i-th edge
-		double Z = A * ((theta_i - theta_ip1) + B * Math.log(Math.pow(ri_2,0.5)/Math.pow(rip1_2,0.5)));
+		double Z = A * ((theta_ip1 - theta_i) + B * Math.log(Math.pow(ri_2,0.5)/Math.pow(rip1_2,0.5)));
 		return Z;
 	}
 	
@@ -216,7 +216,7 @@ public class GeologicModel {
 	public ArrayList<ModelCoord> getModifiedCoord(int j, ArrayList<ModelCoord> mc) {
 		ArrayList<ModelCoord> t = new ArrayList();
 		for (int i = 0; i < mc.size(); i++) {
-			double x = -1 * (mc.get(i).getX() - stations.get(j).getX());
+			double x = (mc.get(i).getX() - stations.get(j).getX());
 			double z = mc.get(i).getZ();
 			ModelCoord temp = new ModelCoord(x,z);
 			t.add(temp);
